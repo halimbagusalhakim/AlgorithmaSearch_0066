8434c7ad1f36fc39014d928937f0eb3dd8439794
halimbagusalhakim <halsat132@gmail.com>
2024-04-05 14:17:45 +0700

    menlanjutkan void input dengan menulis kode untuk menginput element

--- AlgorithmaSearch_0066.cpp
+++ AlgorithmaSearch_0066.cpp
@@ -17,10 +17,18 @@ void input() {
         else
             cout << "\nArray sould have minimum 1 and maximum 20 elements.\n\n";
     }
-}
+
+    cout << "\n-----------------\n";
+    cout << " Enter array elements\n";
+    cout << "-------------------\n";
+    for (i = 0; i < n; i++) {
+        cout << "<" << (i + 1) << "> ";
+        cin >> arr[i];
+    }
+}   
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
