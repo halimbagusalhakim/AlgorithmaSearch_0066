15ec1746fc787f90ccc3b43f40111518dcf5f8ad
halimbagusalhakim <halsat132@gmail.com>
2024-04-05 14:14:05 +0700

    mengisi void input untuk menentukan panjang array

--- AlgorithmaSearch_0066.cpp
+++ AlgorithmaSearch_0066.cpp
@@ -8,6 +8,17 @@ int n;
 int i;
 
 
+void input() {
+    while (true) {
+        cout << " Enter the numver of elements in the array: ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray sould have minimum 1 and maximum 20 elements.\n\n";
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
